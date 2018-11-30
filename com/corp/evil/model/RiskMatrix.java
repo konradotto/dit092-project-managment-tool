5df259e346bfaf13031b5771da5dd8e387c66d44
SpaghettiCode <gusottko@student.gu.se>
2018-11-30 12:29:52 +0100

    add constructor from Set of Risks, add functions to add one or multiple risks

--- com/corp/evil/model/RiskMatrix.java
+++ com/corp/evil/model/RiskMatrix.java
@@ -1,9 +1,31 @@
 import java.util.HashSet;
+import java.util.Set;
 
 public class RiskMatrix {
     private HashSet<Risk> risks;
 
-    public RiskMatrix() {
+    RiskMatrix() {
         risks = new HashSet<>();
+    }
+
+    RiskMatrix(Set<Risk> risks) {
+        this();
+        addRisks(risks);
+    }
+
+    // TODO: Constructor from JSON-file
+
+    void addRisk(Risk risk) {
+        if (risk == null) {
+            throw new IllegalArgumentException("RiskMatrix can not add NULL value to risks");
+        }
+
+        this.risks.add(risk);
+    }
+
+    void addRisks(Set<Risk> risks) {
+        for(Risk risk:risks) {
+            addRisk(risk);
+        }
     }
 }
