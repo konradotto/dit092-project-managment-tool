ad2dcffadaed89310e2658faa62249a208f1faaf
SpaghettiCode <gusottko@student.gu.se>
2018-12-02 00:40:47 +0100

    add value map to RiskProbability

--- com/corp/evil/risk/RiskProbability.java
+++ com/corp/evil/risk/RiskProbability.java
@@ -1,3 +1,7 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public enum RiskProbability {
     VERY_UNLIKELY(1, "very unlikely"),
     UNLIKELY(2, "unlikely"),
@@ -5,19 +9,36 @@ public enum RiskProbability {
     LIKELY(4, "likely"),
     VERY_LIKELY(5, "very likely");
 
-    private int value;
-    private String word;
+    private final int probability;
+    private final String text;
 
-    RiskProbability(int value, String word) {
-        this.value = value;
-        this.word = word;
+    private final static Map<Integer, RiskProbability> map;
+
+    static {
+        HashMap<Integer, RiskProbability> tempMap = new HashMap<>();
+        for (RiskProbability prob : RiskProbability.values()) {
+            tempMap.put(prob.getProbability(), prob);
+        }
+        map = Collections.unmodifiableMap(tempMap);
     }
 
-    public String getWord() {
-        return word;
+    RiskProbability(int probability, String text) {
+        this.probability = probability;
+        this.text = text;
     }
 
-    public int getValue() {
-        return value;
+    public int getProbability() {
+        return probability;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public static RiskProbability valueOf(int probability) throws RiskProbabilityNotDefinedException {
+        if (!map.containsKey(probability)) {
+            throw new RiskProbabilityNotDefinedException("No RiskProbability exists for the suggested probability.");
+        }
+        return map.get(probability);
     }
 }
