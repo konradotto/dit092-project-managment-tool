1d40def3cd47a5f23e499b0ee856fdc731f82486
KardoMarof <gusmaroka@student.gu.se>
2018-12-01 17:43:52 +0100

    new todo

--- com/corp/evil/team/Member.java
+++ com/corp/evil/team/Member.java
@@ -1,9 +1,32 @@
+import java.util.*;
+import java.time.Duration;
+
+
 public class Member {
 
-    private  String name;
-    private UUID ID;
-    //TODO constructor
+    private String name;
+    private final UUID ID = null;
+    private final double SALARY_PER_HOUR;
+    private Duration timeSpent;
 
-    //TODO constructor
+    Member(String name, double salary){
+        this.name = name;
+        this.SALARY_PER_HOUR = salary;
+    }
 
+    public double calculateSalary(){
+
+        return 0;
+    }
+
+    public void spendTime(Duration time){
+    }
+
+    //GETTERS AND SETTERS
+    public String getName() {return name;}
+    public UUID getID() {return ID;}
+    public double getSALARY_PER_HOUR() {return SALARY_PER_HOUR;}
+    public void setName(String name) {this.name = name;}
+    public void setTimeSpent(Duration timeSpent) {this.timeSpent = timeSpent;}
+    public Duration getTimeSpent() {return timeSpent;}
 }
