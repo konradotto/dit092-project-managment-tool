27f84500b4f099b7b7c3848bae9bdfeb8578c4a6
SpaghettiCode <gusottko@student.gu.se>
2018-12-02 01:11:45 +0100

    implement tabular output of RiskMatrix

--- com/corp/evil/risk/RiskMatrix.java
+++ com/corp/evil/risk/RiskMatrix.java
@@ -1,10 +1,12 @@
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import com.corp.evil.RiskAlreadyRegisteredException;
-
 public class RiskMatrix {
+    private final static int COLUMN_WIDTH = 15;
+    private final static int COLUMNS = 4;
+
     private ArrayList<Risk> risks;
     private String lineSeparator = System.lineSeparator();
 
@@ -32,15 +34,31 @@ public class RiskMatrix {
         if (risks.isEmpty()) {
             sb.append("There are no risks registered in this Risk Matrix yet." + lineSeparator);
         } else {
-            sb.append("\tRisk Matrix" + lineSeparator + lineSeparator);
+            sb.append("\tRisk Matrix" + lineSeparator);
 
-            sb.append("Risk name\tprobability\timpact\trisk" + lineSeparator);
+            // format table content
+            sb.append(formatTableRow(new String[] {"Risk name:", "Probability:", "Impact:", "Risk:"}));
+
+            // separator line
+            sb.append(String.join("", Collections.nCopies(COLUMNS * COLUMN_WIDTH, "-")));
+
             for (Risk risk : risks) {
-                sb.append(risk.getRiskName() + "\t" + risk.getProbability() + "\t" + risk.getImpact() +
-                        "\t" + risk.getRisk() + lineSeparator);
+                sb.append(formatTableRow(new String[] {risk.getRiskName(), String.valueOf(risk.getProbability()),
+                        String.valueOf(risk.getImpact()), String.valueOf(risk.getRisk())}));
             }
         }
         return sb.toString();
+    }
+
+    private String formatTableRow(String[] columns) {
+        String result = "";
+
+        for(int i = 0; i < COLUMNS; ++i) {
+            result += String.format("%1$" + COLUMN_WIDTH + "s", columns[i]);
+        }
+        result += String.format("%n");
+
+        return result;
     }
 
     // TODO: Constructor from JSON-file
@@ -52,7 +70,7 @@ public class RiskMatrix {
             throw new RiskAlreadyRegisteredException("A risk with this name has already been registered. " +
                     "Change the name or delete the current risk of name <" + risk.getRiskName() + ">");
         } else {
-        	this.risks.add(risk);
+            this.risks.add(risk);
         }
     }
 
@@ -65,7 +83,7 @@ public class RiskMatrix {
     }
 
     public void addRisks(List<Risk> risks) throws RiskAlreadyRegisteredException, RiskIsNullException {
-        for(Risk risk:risks) {
+        for (Risk risk : risks) {
             addRisk(risk);
         }
     }
