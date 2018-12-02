17fddc5e677039999e4337b2dc150a846865b012
SpaghettiCode <gusottko@student.gu.se>
2018-12-02 02:08:41 +0100

    useless commit adding a comment

--- com/corp/evil/exceptions/RiskAlreadyRegisteredException.java
+++ com/corp/evil/exceptions/RiskAlreadyRegisteredException.java
@@ -2,4 +2,5 @@ public class RiskAlreadyRegisteredException extends Exception {
     public RiskAlreadyRegisteredException(String message) {
         super(message);
     }
+    //TODO: just testing
 }
