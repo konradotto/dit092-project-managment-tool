5e756f0fbf7bbea675d6480e8c5df86a0b26ca66
ih8c01d <jeanpaull@live.it>
2018-12-02 18:44:57 +0100

    UUID, Duration, Calculate Saalary

--- com/corp/evil/team/Member.java
+++ com/corp/evil/team/Member.java
@@ -1,32 +1,35 @@
-import java.util.*;
 import java.time.Duration;
-
+import java.util.UUID;
 
 public class Member {
 
     private String name;
-    private final UUID ID = null;
+    private final String uuid;
     private final double SALARY_PER_HOUR;
     private Duration timeSpent;
 
     Member(String name, double salary){
         this.name = name;
         this.SALARY_PER_HOUR = salary;
+        this.uuid = UUID.randomUUID().toString();
+        this.timeSpent = Duration.ofHours(0);
     }
 
     public double calculateSalary(){
 
-        return 0;
+        return getTimeSpent()*getSALARY_PER_HOUR();
     }
 
-    public void spendTime(Duration time){
+    public void spendTime(long hours){
+        Duration spendTime = timeSpent.plusHours(hours);
+        setTimeSpent(spendTime);
     }
 
     //GETTERS AND SETTERS
     public String getName() {return name;}
-    public UUID getID() {return ID;}
+    public String getID() {return uuid;}
     public double getSALARY_PER_HOUR() {return SALARY_PER_HOUR;}
     public void setName(String name) {this.name = name;}
     public void setTimeSpent(Duration timeSpent) {this.timeSpent = timeSpent;}
-    public Duration getTimeSpent() {return timeSpent;}
+    public long getTimeSpent() {return timeSpent.toHours();}
 }
