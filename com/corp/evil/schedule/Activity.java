fe73611e268beffebbd46b50eaeb50961960a76d
cutie <cutie@10.0.48.19>
2018-12-03 13:36:58 +0100

    created activity constructor

--- com/corp/evil/schedule/Activity.java
+++ com/corp/evil/schedule/Activity.java
@@ -3,5 +3,16 @@ public class Activity {
 	private int startWeek;
 	private int endWeek;
 	private String name;
-	//Marcus was here
+	private Team team;
+	private Budget budget;
+	
+	public Activity(String name, int startWeek, int endWeek, Team team, Budget budget) {
+		this.name = name;
+		this.startWeek = startWeek;
+		this.endWeek = endWeek;
+		this.team = team;
+		this.budget = budget;
+	}
+	
+	
 }
