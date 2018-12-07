26e95f1bf780e3629c9ebf875e463f3ef73d9396
ih8c01d <jeanpaull@live.it>
2018-12-07 16:53:27 +0100

    read list from json method

--- com/corp/evil/team/Team.java
+++ com/corp/evil/team/Team.java
@@ -16,7 +16,6 @@ public class Team {
 
     public Team(){
         members = new ArrayList<>();
-        activities = new ArrayList<>();
         name = "";
 
     }
@@ -104,14 +103,9 @@ public class Team {
     public void setName(String name) {this.name = name;}
 
 
-
-
-
-
-
-    public List<Member> readJsonStreamMembers(Gson gson , InputStream in) throws IOException {
+    public List<Member> readJsonStreamMembers(Gson gson , InputStream in) throws IOException{
         JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
-        List<Member> members = new ArrayList<Member>();
+        List<Member> members = new ArrayList<>();
         reader.beginArray();
         while (reader.hasNext()) {
             Member member = gson.fromJson(reader, Member.class);
@@ -120,53 +114,6 @@ public class Team {
         reader.endArray();
         reader.close();
         return members;
-    }
-
-
-    static void prettyprint(JsonReader reader, JsonWriter writer) throws IOException {
-        while (true) {
-            JsonToken token = reader.peek();
-            switch (token) {
-                case BEGIN_ARRAY:
-                    reader.beginArray();
-                    writer.beginArray();
-                    break;
-                case END_ARRAY:
-                    reader.endArray();
-                    writer.endArray();
-                    break;
-                case BEGIN_OBJECT:
-                    reader.beginObject();
-                    writer.beginObject();
-                    break;
-                case END_OBJECT:
-                    reader.endObject();
-                    writer.endObject();
-                    break;
-                case NAME:
-                    String name = reader.nextName();
-                    writer.name(name);
-                    break;
-                case STRING:
-                    String s = reader.nextString();
-                    writer.value(s);
-                    break;
-                case NUMBER:
-                    String n = reader.nextString();
-                    writer.value(new BigDecimal(n));
-                    break;
-                case BOOLEAN:
-                    boolean b = reader.nextBoolean();
-                    writer.value(b);
-                    break;
-                case NULL:
-                    reader.nextNull();
-                    writer.nullValue();
-                    break;
-                case END_DOCUMENT:
-                    return;
-            }
-        }
     }
 
 }
