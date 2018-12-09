eab0b1a824f9d4715ba3213f6b93efecbf6bb67e
SpaghettiCode <gusottko@student.gu.se>
2018-12-09 20:27:06 +0100

    add default constructor to Budget

--- com/corp/evil/budget/Budget.java
+++ com/corp/evil/budget/Budget.java
@@ -2,19 +2,28 @@ import java.util.List;
 
 public class Budget {
 
+    private final static double DEFAULT_COST_SCHEDULED = 0;
+    private final static double DEFAULT_COST_PERFORMED = 0;
+    private final static double DEFAULT_PERCENT_COMPLETED = 0;
+
     private double costOfWorkScheduled;
     private double costOfWorkPerformed;
+    private double percentCompleted;
+
+    private double earnedValue;
+
+
+    public Budget(double costScheduled, double costPerformed, double percentCompleted) {
+        this.costOfWorkScheduled = costScheduled;
+        this.costOfWorkPerformed = costPerformed;
+        this.percentCompleted = percentCompleted;
+    }
 
     public Budget() {
-        this.costOfWorkScheduled = 0;
-        this.costOfWorkPerformed = 0;
+        this(DEFAULT_COST_SCHEDULED, DEFAULT_COST_PERFORMED, DEFAULT_PERCENT_COMPLETED);
     }
 
     public Budget(List<Budget> budgets) {
 
     }
-
-
-
-
 }
