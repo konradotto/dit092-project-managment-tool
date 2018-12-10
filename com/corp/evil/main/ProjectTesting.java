aca595944c322af4c859395ff5b55d50ff233a0d
SpaghettiCode <gusottko@student.gu.se>
2018-12-10 13:17:26 +0100

    test project

--- com/corp/evil/main/ProjectTesting.java
+++ com/corp/evil/main/ProjectTesting.java
@@ -2,12 +2,21 @@ import java.util.ArrayList;
 
 public class ProjectTesting {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RiskAlreadyRegisteredException, RiskIsNullException, RiskProbabilityNotDefinedException, RiskImpactNotDefinedException {
         ProjectSchedule schedule = new ProjectSchedule(2018, 48, 2019, 3,
                 new ArrayList<Activity>());
 
-        Project broject = new Project("Broject", new Team(), new RiskMatrix(), schedule, new Budget());
+        Project broject = new Project("", new Team(), new RiskMatrix(), schedule, new Budget());
 
+        System.out.println(broject);
+
+        Risk r1 = new Risk("Death", 5, 5);
+        broject.getRiskMatrix().addRisk(r1);
+
+        System.out.println(broject);
+
+        Budget b1 = new Budget(2000, 3000, 75);
+        broject.setBudget(b1);
         System.out.println(broject);
     }
 }
