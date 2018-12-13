93852127d429d31d83e666821d86afee046419b3
SpaghettiCode <gusottko@student.gu.se>
2018-12-14 00:39:04 +0100

    move Budget Functionalities to Activity

--- com/corp/evil/schedule/Activity.java
+++ com/corp/evil/schedule/Activity.java
@@ -1,74 +1,135 @@
+import java.util.Calendar;
+
 public class Activity {
-	
-	private int startWeek;
-	private int endWeek;
-	private String name;
-	private Team team;
-	private Budget budget;
-	private final int LAST_WEEK_OF_YEAR = 52;
+
+    private int startWeek;
+    private int endWeek;
+    private String name;
+    private Team team;
+    private Budget budget;
+    private final static int LAST_WEEK_OF_YEAR = 52;
+    private final static int DAYS_IN_WORKWEEK = 5;
+
+    private double costOfWorkScheduled;
+    private double costOfWorkPerformed;
+    private double percentCompleted;
 
 
-	public Activity(String name, int startWeek, int endWeek, Team team, Budget budget) {
-		this.name = name;
-		this.startWeek = startWeek;
-		this.endWeek = endWeek;
-		this.team = team;
-		this.budget = budget;
-	}
+    public Activity(String name, int startWeek, int endWeek, Team team) {
+        this.name = name;
+        this.startWeek = startWeek;
+        this.endWeek = endWeek;
+        this.team = team;
+        this.costOfWorkScheduled = scheduledCost();
+        this.costOfWorkPerformed = 0.0;
+        this.percentCompleted = 0.0;
+
+        try {
+            team.addActivity(this);
+        } catch (ActivityAlreadyRegisteredException e) {
+            e.printStackTrace();
+        } catch (ActivityIsNullException e) {
+            e.printStackTrace();
+        }
+    }
 
     //TODO: activity constructor without team
 
-	//Accessor methods
-	public int getDuration(){
-		int durationPerWeek;
-		if (getStartWeek()>getEndWeek()){
-			int firstDur = LAST_WEEK_OF_YEAR - getStartWeek();
-			 durationPerWeek = firstDur + getEndWeek();
-		} else {
-			 durationPerWeek =  getEndWeek() - getStartWeek();
-		}
-		int durationPerHour = durationPerWeek * 20;
-		return durationPerHour;
-	}
+    //Accessor methods
+    public int getDuration() {
+        int weeks;
+        if (getStartWeek() > getEndWeek()) {
+            int firstDur = LAST_WEEK_OF_YEAR - getStartWeek();
+            weeks = firstDur + getEndWeek();
+        } else {
+            weeks = getEndWeek() - getStartWeek();
+        }
+        int hours = weeks * 20;
+        return hours;
+    }
 
-	public int getStartWeek() {
-		return startWeek;
-	}
+    public double getPercentCompleted() {
+        return percentCompleted;
+    }
 
-	public void setStartWeek(int startWeek) {
-		this.startWeek = startWeek;
-	}
+    public double getCostOfWorkScheduled() {
+        return costOfWorkScheduled;
+    }
 
-	public int getEndWeek() {
-		return endWeek;
-	}
+    public double getCostOfWorkPerformed() {
+        return costOfWorkPerformed;
+    }
 
-	public void setEndWeek(int endWeek) {
-		this.endWeek = endWeek;
-	}
+    public double getPercentScheduled() {
+        Calendar cal = Calendar.getInstance();
+        int week = cal.get(Calendar.WEEK_OF_YEAR);
+        int day = cal.get(Calendar.DAY_OF_WEEK);
 
-	public String getName() {
-		return name;
-	}
+        return getPercentSchedule(week, day);
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public double getPercentSchedule(int week, int day) {
+        int totalWeeks = (endWeek >= startWeek) ? (endWeek - startWeek + 1) :
+                (endWeek + LAST_WEEK_OF_YEAR) - startWeek + 1;
+        int weeksPassed = (week >= startWeek) ? week - startWeek : (week + LAST_WEEK_OF_YEAR) - startWeek;
 
-	public Team getTeam() {
-		return team;
-	}
+        int daysPassed = (day < Calendar.SATURDAY) ? (day - Calendar.SUNDAY) : DAYS_IN_WORKWEEK;
 
-	public void setTeam(Team team) {
-		this.team = team;
-	}
+        return (weeksPassed + (daysPassed / DAYS_IN_WORKWEEK)) / totalWeeks;
+    }
 
-	public Budget getBudget() {
-		return budget;
-	}
+    public double scheduledCost() {
+        double averageSalary = 0.0;
+        int teamSize = team.getMembers().size();
+        for (Member member : team.getMembers()) {
+            averageSalary += member.getSALARY_PER_HOUR();
+        }
+        return (averageSalary /= teamSize) * getDuration();
+    }
 
-	public void setBudget(Budget budget) {
-		this.budget = budget;
-	}
-	//End of Accessor methods
+    public void spendTime(long timeScheduled, double cost) {
+        costOfWorkPerformed += cost;
+        percentCompleted += ((double) timeScheduled) / ((double) getDuration());
+    }
+
+    public int getStartWeek() {
+        return startWeek;
+    }
+
+    public void setStartWeek(int startWeek) {
+        this.startWeek = startWeek;
+    }
+
+    public int getEndWeek() {
+        return endWeek;
+    }
+
+    public void setEndWeek(int endWeek) {
+        this.endWeek = endWeek;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public Budget getBudget() {
+        return budget;
+    }
+
+    public void setBudget(Budget budget) {
+        this.budget = budget;
+    }
+    //End of Accessor methods
 }
