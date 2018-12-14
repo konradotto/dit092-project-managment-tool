4023e05d34729690622a8ed8305129b8c28effb6
Turbohink <jakob@jkarlsson.se>
2018-12-14 16:34:59 +0100

    merged project schedule.

---
+++ com/corp/evil/main/Print.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class Print{
+
+    private static StringBuilder sb = new StringBuilder();
+    private static String newline = System.lineSeparator();
+    Scanner input = new Scanner(System.in);
+
+    public static int printStartMenu() {
+
+        sb.append("Welcome to the Project Planning Software of Evil Corp"+newline);
+        sb.append("Press one of the follwing options from the list"+newline);
+        sb.append("➤ 1. Load a existing project"+newline);
+        sb.append("➤ 2. Create an empty project");
+
+
+        System.out.println(sb);
+        sb.setLength(0);
+
+        return input.nextInt();
+    }
+
+    public static int printEditing() {
+
+
+        sb.append("Choose what you want to edit"+newline);
+        sb.append("➤ 1. Project Name"+newline);
+        sb.append("➤ 2. Project Team"+newline);
+        sb.append("➤ 3. Project riskMatrix"+newline);
+        sb.append("➤ 4. Project Schedule"+newline);
+        sb.append("➤ 5. Exit"+newline);
+
+        System.out.println(sb);
+        sb.setLength(0);
+
+        return input.nextInt();
+    }
+
+    public static String printEditingName() {
+
+        sb.append("Please enter a new name for your project: "+newline);
+
+        System.out.println(sb);
+        sb.setLength(0);
+
+        return input.nextLine();
+    }
+
+    public static int printEditingTeam() {
+
+        sb.append("Choose what you want to edit"+newline);
+        sb.append("➤ 1. Add member "+newline);
+        sb.append("➤ 2. Remove member "+newline);
+
+        System.out.println(sb);
+        sb.setLength(0);
+
+        return input.nextInt();
+    }
+
+    public static int printEditingRiskMatrix() {
+
+        sb.append("Choose what you want to edit"+newline);
+        sb.append("➤ 1. Add risk"+newline);
+        sb.append("➤ 2. Remove risk"+newline);
+
+        System.out.println(sb);
+        sb.setLength(0);
+
+        return input.nextInt();
+    }
+
+    public static int printEditingSchedule() {
+
+        sb.append("Choose what you want to edit"+newline);
+        sb.append("➤ 1. Add activity"+newline);
+        sb.append("➤ 2. Remove activity"+newline);
+
+
+        System.out.println(sb);
+        sb.setLength(0);
+
+        return input.nextInt();
+    }
+}
+
