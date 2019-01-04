77223d19544a055bcf06982d87bbc1f42a9e837e
SpaghettiCode <gusottko@student.gu.se>
2019-01-04 19:26:35 +0100

    fix code-formatting of myScanner (spaces, tabs, etc.)

--- com/corp/evil/main/myScanner.java
+++ com/corp/evil/main/myScanner.java
@@ -2,74 +2,74 @@ class myScanner {
 
     private static java.util.Scanner input = new java.util.Scanner(System.in);
 
-    static String readLine(String string){
+    static String readLine(String string) {
         System.out.println(string);
         String x = input.nextLine();
         while (x.trim().isEmpty()) {
             System.out.println(string);
-            x=input.nextLine();
+            x = input.nextLine();
         }
         return x;
     }
 
-    static double readDouble(String string){
+    static double readDouble(String string) {
 
         boolean result;
-        double x =0;
+        double x = 0;
         do {
             System.out.println(string);
             String y = input.nextLine();
             while (y.trim().isEmpty()) {
                 System.out.println(string);
-                y=input.nextLine();
+                y = input.nextLine();
             }
             try {
                 x = Double.parseDouble(y);
                 return x;
-            }catch (java.lang.NumberFormatException e){
-                result =false;
+            } catch (java.lang.NumberFormatException e) {
+                result = false;
             }
-        }while (!result);
+        } while (!result);
         return x;
     }
 
-    static int readInt(String string){
+    static int readInt(String string) {
         boolean result;
-        int x =0;
+        int x = 0;
         do {
             System.out.println(string);
             String y = input.nextLine();
             while (y.trim().isEmpty()) {
                 System.out.println(string);
-                y=input.nextLine();
+                y = input.nextLine();
             }
             try {
                 x = Integer.parseInt(y);
                 return x;
-            }catch (java.lang.NumberFormatException e){
-                result =false;
+            } catch (java.lang.NumberFormatException e) {
+                result = false;
             }
-        }while (!result);
+        } while (!result);
         return x;
     }
 
-    static int readInt(){
+    static int readInt() {
         boolean result;
-        int x =0;
+        int x = 0;
         do {
-        String y = input.nextLine();
-        while (y.trim().isEmpty()) {
-            System.out.println("Please enter an integer!");
-            y=input.nextLine();
-        }
-            try {
-                 x = Integer.parseInt(y);
-                return x;
-            }catch (java.lang.NumberFormatException e){
+            String y = input.nextLine();
+            while (y.trim().isEmpty()) {
                 System.out.println("Please enter an integer!");
-                result =false;
+                y = input.nextLine();
             }
-        }while (!result);
+            try {
+                x = Integer.parseInt(y);
+                return x;
+            } catch (java.lang.NumberFormatException e) {
+                System.out.println("Please enter an integer!");
+                result = false;
+            }
+        } while (!result);
         return x;
     }
 
